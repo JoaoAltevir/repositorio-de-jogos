9c43ac5fbb8062cbbb30cfe4680ae22242e523b3
João Neto <j.altevirneto@gmail.com>
2024-09-10 08:13:58 -0300

    feat: feito interação com usuario

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const estudioRouter = require("./routes/estudioRouter.js");
+const generoRouter = require("./routes/generoRouter.js");
+const publicadoraRouter = require("./routes/publicadoraRouter.js");
+const jogoRouter = require("./routes/jogoRouter.js");
 
-app.use("")
+app.use(express.json());
+
+app.use("/estudio", estudioRouter);
+app.use("/genero", generoRouter);
+app.use("/publicadora", publicadoraRouter);
+app.use("/jogo", jogoRouter);
+
+app.listen(port, () => {
+    console.log(`listening on ${port} port`);
+});
